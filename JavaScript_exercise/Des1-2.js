ae32c390f3b9a9ccac35136a69d24098e1d53b56
Andre Rodrigues Lima <andrezaga9@hotmail.com>
2020-02-01 02:41:33 +1100

    Javascript
    Exercise

---
+++ JavaScript_exercise/Des1-2.js
@@ -0,0 +1,26 @@
+/*const empresa = {
+    nome: 'Rocketseat',
+    Cor:'Roxo',
+    Foco: 'Programacao',
+        Endereco:{
+            Rua: 'Rua Guilherme Gembala',
+            numero: 260
+    
+        },
+    
+    }
+    console.log(`A empresa ${empresa.nome} esta localizada em ${empresa.Endereco.Rua}, ${empresa.Endereco.numero}`)
+*/
+    const pessoa = {
+        nome: 'Carlos',
+        idade: 32,
+            tecnologia:[
+                { nome: 'C++', especialidade: 'Desktop' }, 
+                { nome: 'JavaScript', especialidade: 'Web/Mobile' }
+
+
+            ],
+
+
+    }
+    console.log(`${pessoa.nome} tem ${pessoa.idade} e usa a tecnologia ${pessoa.tecnologia[0].nome} com especialiadade em ${pessoa.tecnologia[0].especialidade}`)
---
+++ JavaScript_exercise/Des1-3.js
@@ -0,0 +1,33 @@
+// const usuarios = [
+//     { nome: 'Carlos', tecnologias: ['HTML','CSS'] },
+//     { nome: 'Jasmine', tecnologias: ['JavaScript', 'CSS'] },
+//     { nome: 'Tuane', tecnologias: ['HTML', 'Node.js'] }
+//   ]
+
+// //   for(let i=0; i<usuarios.length; i++){
+// //       console.log(`${usuarios[i].nome} trabalha com ${usuarios[i].tecnologias}`)
+
+// // parte1  }
+// function checaSeUsuarioUsaCSS(usuario) {
+
+// let tec = usuario.tecnologias
+
+// for(let t=0; t<usuario.tecnologias.length; t++){
+// if (tec[t]=='CSS'){
+//     return true
+// }
+
+
+
+// }
+
+// }
+
+// for (let i = 0; i < usuarios.length; i++) {
+//     const usuarioTrabalhaComCSS = checaSeUsuarioUsaCSS(usuarios[i])
+  
+//     if(usuarioTrabalhaComCSS) {
+//       console.log(`O usuário ${usuarios[i].nome} trabalha com CSS`)
+//     }
+//   }
+//   //parte2
