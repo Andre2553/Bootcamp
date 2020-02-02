f0f9f5f1f0d21c8dc78202dfaa224aed53bcb720
Andre Rodrigues Lima <andrezaga9@hotmail.com>
2020-02-02 21:42:52 +1100

    Blocks
    Front-end to Back-end

--- server_bootcamp/server.js
+++ server_bootcamp/server.js
@@ -7,11 +7,29 @@ server.set("view engine", "html")
 
 server.use(express.static('public'))
 nunjucks.configure("views",{
-express:server
+express:server,
+autoescape: false
 })
 
 server.get("/", function(req,res){
-    return res.render("about.njk")
+  const about = {
+    title: "Rocketseat",
+   tec:[
+    {name:"JavaScript"},
+    {name:"CSS"},
+    {name:"HTML"},
+    {name:"React"},
+    {name:"ReactNative"}
+   ],
+    links: [
+        {name:"GitHub", url:"https://github.com/Rocketseat"},
+        {name:"Instagram", url:"https://www.instagram.com/rocketseat_oficial/"},
+        {name:"Facebook", url:"https://www.facebook.com/rocketseat"},
+       
+    ]
+
+  }
+    return res.render("about.njk",{ about })
 })
 
 server.get("/portifolio", function(req,res){
