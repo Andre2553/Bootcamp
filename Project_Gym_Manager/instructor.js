75b2b8c48c5a95f6b1095ad4d0205d5b6a159ac5
Andre Rodrigues Lima <andrezaga9@hotmail.com>
2020-04-05 13:45:06 +1000

    Creating a new instructor profile
    Creating an option to make a new instructor profile, using JSON to get the data from the route instructors/create

--- Project_Gym_Manager/instructor.js
+++ Project_Gym_Manager/instructor.js
@@ -1,10 +1,18 @@
+const fs = require('fs')
+const data = require('./data.json')
 exports.post = function(req,res){
-    
+        
         const keys = Object.keys(req.body)
         for(key of keys){
             if(req.body[key]==""){
                 return res.send('Please, fill all fields')
             }
         }
-        return res.send(keys)
+        data.instructors.push(req.body)
+        fs.writeFile("data.json",JSON.stringify(data,null,2),function(err){
+            if(err)return res.send("Write file error")
+
+            return res.redirect("/instructors")
+        })
+        //return res.send(req.body)
     }
