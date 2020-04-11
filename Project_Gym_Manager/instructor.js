ed32ad29fd3050704e83460719e508c64993506e
Andre Rodrigues Lima <andrezaga9@hotmail.com>
2020-04-11 23:31:03 +1000

    JSON message
    Using json to pass information to the backend

--- Project_Gym_Manager/instructor.js
+++ Project_Gym_Manager/instructor.js
@@ -8,7 +8,21 @@ exports.post = function(req,res){
                 return res.send('Please, fill all fields')
             }
         }
-        data.instructors.push(req.body)
+        
+        
+        let{avatar_url,birth,name,services,gender}=req.body
+        birth = Date.parse(birth)
+        const created_at = Date.now()
+        const id = Number(data.instructors.length + 1)
+        data.instructors.push({
+            id,
+            avatar_url,
+            name,
+            birth,
+            gender,
+            services,
+            created_at,
+        })
         fs.writeFile("data.json",JSON.stringify(data,null,2),function(err){
             if(err)return res.send("Write file error")
 
